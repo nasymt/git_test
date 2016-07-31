772591883be376d420da9f3778255865ebffe378
mtok <TadashiMatsuoka@MacBook-Pro.local>
2016-07-31 22:03:14 +0900

    my classクラス追加

--- git_test/main.cpp
+++ git_test/main.cpp
@@ -7,9 +7,24 @@
 //
 
 #include <iostream>
+using namespace std;
+
+class myclass{
+    int a;
+    public:
+    myclass(int x);
+    int get();
+};
+
+myclass::myclass(int x){
+    a = x;
+}
+
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    cout << "Hello, World!\n";
+    
+    
     return 0;
 }
