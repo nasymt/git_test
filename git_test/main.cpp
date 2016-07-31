7fc1c4f883a04a286a161dc81ac8e2adf14307cd
mtok <TadashiMatsuoka@macbook-pro.local>
2016-07-31 20:47:15 +0900

    Initial Commit

---
+++ git_test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  git_test
+//
+//  Created by 松岡正 on 2016/07/31.
+//  Copyright © 2016年 松岡正. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
